5592771de22471d67981f00404118e8345f8b7f1
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-15 15:53:30 +0100

    add POST create image

--- app/images/serializers.py
+++ app/images/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+from .models import Image
 
 
 class ImageSerializer(serializers.Serializer):
     image = serializers.ImageField(use_url=True)
 
     class Meta:
-        fields = ['original_url']
+        fields = ['image']
+
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['user']
+        return Image.objects.create(**validated_data)
--- app/images/views.py
+++ app/images/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Image
@@ -10,6 +10,17 @@ class ImageViewSet(viewsets.ViewSet):
     A simple viewset to list all images
     """
     permission_classes = [IsAuthenticated]
+    serializer_class = ImageSerializer
+
+    def create(self, request):
+        serializer = ImageSerializer(data=request.data, context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+        pass
 
     def list(self, request):
         serializer = ImageSerializer(self.get_queryset(), context={'request': request}, many=True)
