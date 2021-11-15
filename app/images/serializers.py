8ccc59916c3ff76393176a60eed2d0ca7605be5c
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-15 20:18:50 +0100

    add validation for image filetype

--- app/images/serializers.py
+++ app/images/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Image
+import PIL
 
 
 class ImageSerializer(serializers.Serializer):
@@ -11,3 +12,11 @@ class ImageSerializer(serializers.Serializer):
     def create(self, validated_data):
         validated_data['owner'] = self.context['user']
         return Image.objects.create(**validated_data)
+
+    def validate_image(self, value):
+        """
+        Make sure the image is jpg/png format
+        """
+        if value.content_type not in ['image/jpeg', 'image/png']:
+            raise serializers.ValidationError(f'{value.content_type} is not supported')
+        return value
