436abfeb2fcaf851d90f405f2407bc3596e40fc1
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-11-21 19:21:59 +0100

    refactor APITestCaseWithMedia class

--- app/images/tests.py
+++ app/images/tests.py
@@ -18,7 +18,7 @@ client = APIClient()
 class APITestCaseWithMedia(APITestCase):
     """
     class adds temporary directory for media files that gets deletes automatically after tests
-    tests that uploads media should use:
+    tests that uploads media must use:
     with self.settings(MEDIA_ROOT=self.temporary_dir.name):
     """
     temporary_dir = None
@@ -26,7 +26,6 @@ class APITestCaseWithMedia(APITestCase):
     @classmethod
     def setUpClass(cls):
         cls.temporary_dir = tempfile.TemporaryDirectory()
-
         super(APITestCaseWithMedia, cls).setUpClass()
 
     @classmethod
@@ -34,12 +33,33 @@ class APITestCaseWithMedia(APITestCase):
         cls.temporary_dir = None
         super(APITestCaseWithMedia, cls).tearDownClass()
 
+    def _get_temporary_image(self, size, extension):
+        """
+        creates NamedTemporaryFile and stores image created with PIL in it
+        :param size: (height, width) tuple
+        :param extension: file extension eg. 'jpeg'
+        :return: NamedTemporaryFile object
+        """
+        color = (255, 0, 0)
+        image = PIL_Image.new("RGB", size, color)
+        temp_file = tempfile.NamedTemporaryFile()
+        image.save(temp_file, extension)
+        return temp_file
 
-def get_temporary_image(temp_file, size, extension):
-    color = (255, 0, 0, 0)
-    image = PIL_Image.new("RGB", size, color)
-    image.save(temp_file, extension)
-    return temp_file
+    def _create_image(self, auth, size=(200, 200)):
+        """
+        need to be invoked inside:
+        with self.settings(MEDIA_ROOT=self.temporary_dir.name):
+        :param auth: username, password iterable
+        :param size: height, width tuple
+        :return: newly created Image tuple
+        """
+        client.login(username=auth[0], password=auth[1])
+        owner = User.objects.filter(username=auth[0]).first()
+        test_image = self._get_temporary_image(size, 'jpeg')
+        file = InMemoryUploadedFile(test_image, None, test_image.name, 'image/jpeg', test_image.__sizeof__(), None)
+        Image.objects.create(owner=owner, image=file)
+        client.logout()
 
 
 class GetAllImages(APITestCaseWithMedia):
@@ -49,19 +69,24 @@ class GetAllImages(APITestCaseWithMedia):
     fixtures = ['accounts.json']
 
     def test_get_images_list_unauthorized(self):
+        """
+        request images list as unauthorized user
+        """
         response = client.get(reverse('images-list'))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_images_list_valid(self):
+        """
+        create two images one of them owned by 'admin'. Then get list of all images owned by 'admin'
+        """
         with self.settings(MEDIA_ROOT=self.temporary_dir.name):
+            self._create_image(('seamel', 'ZwpDu9BGHRTTqKX'))
+            self._create_image(('admin', 'admin'))
+
             client.login(username='admin', password='admin')
-            owner = User.objects.filter(username='admin').first()
-            temp_file = tempfile.NamedTemporaryFile()
-            test_image = get_temporary_image(temp_file, (200, 200), 'jpeg')
-            file = InMemoryUploadedFile(test_image, None, test_image.name, 'image/jpeg', test_image.__sizeof__(), None)
-            Image.objects.create(owner=owner, image=file)
             response = client.get(reverse('images-list'))
             client.logout()
 
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertEqual(Image.objects.all().count(), 1)
+            self.assertEqual(len(response.data), 1)
+            self.assertEqual(Image.objects.all().count(), 2)
