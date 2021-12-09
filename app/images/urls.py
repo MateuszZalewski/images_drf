e702791323229082ab4bf3ca2564c62356b5861f
mateuszzalewski <MateuszZalewsky@gmail.com>
2021-12-09 14:00:46 +0100

    switch SimpleRouter to DefaultRouter to provide api-root view

--- app/images/urls.py
+++ app/images/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from rest_framework import routers
 
 from .views import ImageViewSet, media_access, get_thumbnail, \
-    access_expiring, ExpiringLinkViewSet, api_root
+    access_expiring, ExpiringLinkViewSet
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register(r'images', ImageViewSet, basename='images')
 router.register(r'expiring', ExpiringLinkViewSet, basename='expiring')
 urlpatterns = router.urls + [
     path('media/<str:path>', media_access, name='media'),
     path('media/<str:path>/<int:height>', get_thumbnail, name='thumbnail'),
     path('link/<str:name>', access_expiring, name='get-expiring'),
-    path('', api_root, name='api-root')
 ]
